bb1026a842319313b9ca2e229e7d7c90c1e251eb
Andrew Wade <drew.adwade@gmail.com>
2020-12-02 16:44:12 -0500

    working version

--- webgopher.py
+++ webgopher.py
@@ -21,7 +21,7 @@ python3 webgopher.py <inputfile>
 import os
 import sys
 import requests
-#from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -46,6 +46,7 @@ def getHeaders(URL):
 
     CSP = "**MISSING"
     XFO = "**MISSING"
+    XSS = "**MISSING"
     XCTO = "**MISSING"
     RP = "**MISSING"
     PP = "**MISSING"
@@ -53,11 +54,13 @@ def getHeaders(URL):
     BAD = 0
 
     try: 
-        response = requests.get(URL, verify=False)
+        response = requests.head(URL, verify=False)
         if "Content-Security-Policy" in str(response.headers):
             CSP = response.headers['Content-Security-Policy']
         if "X-Frame-Options" in str(response.headers):
             XFO = response.headers['X-Frame-Options']
+        if "X-XSS-Protection" in str(response.headers):
+            XSS = response.headers['X-XSS-Protection']
         if "X-Content-Type-Options" in str(response.headers):
             XCTO = response.headers['X-Content-Type-Options']
         if "Referrer-Policy" in str(response.headers):
@@ -71,12 +74,13 @@ def getHeaders(URL):
     
     CSP = "Content-Security-Policy: " + CSP
     XFO = "X-Frame-Options: " + XFO
+    XSS = "X-XSS-Protection: " + XSS
     XCTO = "X-Content-Type-Options: " + XCTO
     RP = "Referrer-Policy: " + RP
     PP = "Permissions-Policy: " + PP
     STS = "Strict-Transport-Security: " + STS
 
-    Headers = [CSP,XFO,XCTO,RP,PP,STS,BAD]
+    Headers = [CSP,XFO,XSS,XCTO,RP,PP,STS,BAD]
     return Headers
 
 
@@ -125,12 +129,11 @@ def getCookies(URL):
     Secure = "**MISSING Secure flag"
     HTTPOnly = "**MISSING HTTP Only flag"
 
-    response = requests.get(URL, verify=False, headers={'Cache-Control': 'no-cache'})
+    response = requests.get(URL, verify=False)
 
     bad_cookies = []
-
+    
     for cookie in response.cookies:
-        print(cookie)
         if cookie.secure: 
             Secure = " "                  #Secure flag OK
         if cookie.has_nonstandard_attr('HttpOnly'):
@@ -154,64 +157,67 @@ except:
 
 URLs = getURLs.split("\n")
 
-#URLs = ['http://support.gazelle.ai']
-
 chrome_options = Options()  
 chrome_options.add_argument("--headless")  
 chrome_options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'  
 driver = webdriver.Chrome(executable_path=os.path.abspath("/Library/Frameworks/Python.framework/Versions/3.7/bin/chromedriver"), options=chrome_options)
 
-# Output to file
-#try:    
-output_file=open("./reportfile.txt",'a')
-for URL in URLs:
-    output_file.write("###############################################\n")
-    output_file.write(URL)
-    output_file.write("\n###############################################\n")
-    Headers = getHeaders(URL) 
-    if Headers[6] == 0:
-        output_file.write("---SECURITY HEADERS---\n")
-        output_file.write(Headers[0])
-        output_file.write("\n")
-        output_file.write(Headers[1])
-        output_file.write("\n")
-        output_file.write(Headers[2])
-        output_file.write("\n")
-        output_file.write(Headers[3])
-        output_file.write("\n")
-        output_file.write(Headers[4])
-        output_file.write("\n")
-        output_file.write(Headers[5])
-        output_file.write("\n\n")
-        if "https" in str(URL):
-            output_file.write("-- SSL/TLS VERSIONS --\n")
-            TLS = getTLS(URL,driver)
-            for line in TLS:
-                line = line.strip()+"led\n"
-                if "deprecated" in line and "enabled" in line:
-                    line = "**" + line
-                elif "deprecated" not in line and "disabled" in line:                   
-                    line = "**" + line
-                output_file.write(line)
-            output_file.write("\n")
-            output_file.write("---CERTIFICATE CHECK---\n")
-            Cert = getCert(URL)
-            output_file.write(Cert)
-            output_file.write("\n\n")
-        Cookies = getCookies(URL)    
-        if Cookies:
-            output_file.write("---COOKIE SECURITY FLAGS---\n")
-            for Cookie in Cookies:
-                output_file.write(Cookie)
-            output_file.write("\n")
-        
-    else:
-        output_file.write("Site not found.\n\n")
-    output_file.write("\n\n")
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-driver.quit()
-output_file.close()
-print("Success! Your report has been saved to: ./reportfile.txt")
-#except:     
-#    print("Error: The report failed to generate. Check your input file and settings.")
-#    usage()
+
+# Output to file
+try:    
+    output_file=open("./reportfile.txt",'a')
+    for URL in URLs:
+        output_file.write("###############################################\n")
+        output_file.write(URL)
+        output_file.write("\n###############################################\n")
+        Headers = getHeaders(URL) 
+        if Headers[7] == 0:
+            output_file.write("---SECURITY HEADERS---\n")
+            output_file.write(Headers[0])
+            output_file.write("\n")
+            output_file.write(Headers[1])
+            output_file.write("\n")
+            output_file.write(Headers[2])
+            output_file.write("\n")
+            output_file.write(Headers[3])
+            output_file.write("\n")
+            output_file.write(Headers[4])
+            output_file.write("\n")
+            output_file.write(Headers[5])
+            output_file.write("\n")
+            output_file.write(Headers[6])
+            output_file.write("\n\n")
+            if "https" in str(URL):
+                output_file.write("-- SSL/TLS VERSIONS --\n")
+                TLS = getTLS(URL,driver)
+                for line in TLS:
+                    line = line.strip()+"led\n"
+                    if "deprecated" in line and "enabled" in line:
+                        line = "**" + line
+                    elif "deprecated" not in line and "disabled" in line:                   
+                        line = "**" + line
+                    output_file.write(line)
+                output_file.write("\n")
+                output_file.write("---CERTIFICATE CHECK---\n")
+                Cert = getCert(URL)
+                output_file.write(Cert)
+                output_file.write("\n\n")
+            Cookies = getCookies(URL)    
+            if Cookies:
+                output_file.write("---COOKIE SECURITY FLAGS---\n")
+                for Cookie in Cookies:
+                    output_file.write(Cookie)
+                output_file.write("\n")
+            
+        else:
+            output_file.write("Site not found.\n\n")
+        output_file.write("\n\n")
+
+    driver.quit()
+    output_file.close()
+    print("Success! Your report has been saved to: ./reportfile.txt")
+except:     
+    print("Error: The report failed to generate. Check your input file and settings.")
+    usage()
